ee7dc87751987bd92bce96e59a8364d538e0540c
Henrik Smed S <hbssss.hs@gmail.com>
2026-08-18 13:37:36 +0200

    phase1: enable cast receiver resources

--- clients/mobile-tv/android/app/build.gradle.kts
+++ clients/mobile-tv/android/app/build.gradle.kts
@@ -14,6 +14,10 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    buildFeatures {
+        resValues = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
